a768177a5f3eebb108ff10390cba91e414b7c7e3
dirtysmile <dirtysmile89@naver.com>
2021-05-07 09:25:14 +0900

    게임

--- src/book/p96.java
+++ src/book/p96.java
@@ -15,6 +15,14 @@ public class p96 {
          * 3 : 동
          */
         int[] location = {1,1,0};
+        int x = 4;
+        int y = 4;
+
+        List<int[]> move = new ArrayList<>();
+        move.add(new int[]{0,1});
+        move.add(new int[]{-1,0});
+        move.add(new int[]{0,-1});
+        move.add(new int[]{1,0});
 
         List<int[]> map = new ArrayList();
         map.add(new int[]{1,1,1,1});
@@ -30,16 +38,17 @@ public class p96 {
             turnLeft(location);
             switch (location[2]){
                 case 0 :
-                    System.out.println("1");
+                    boolean flg = moveMe(location, move.get(0), x, y, map);
                     break;
                 case 1 :
-                    System.out.println("1");
+                    moveMe(location,move.get(1),x,y,map);
+
                     break;
                 case 2 :
-                    System.out.println("1");
+                    moveMe(location,move.get(2),x,y,map);
                     break;
                 case 3 :
-                    System.out.println("1");
+                    moveMe(location,move.get(3),x,y,map);
                     break;
             }
 
@@ -50,6 +59,49 @@ public class p96 {
             System.out.println(o);
 
         }
+    }
+
+    public static boolean moveMe(int[] location, int[] direct, int x, int y, List map){
+        boolean check1 = checkMap(location,direct,x,y);
+
+        if(!check1){
+            return false;
+        }
+
+        boolean check2 = checkBottom(map,location,direct);
+
+        if(!check2){
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean checkBottom(List<int []> map, int[] location, int[] direct){
+        int x = location[0] + direct[0];
+        int y = location[1] + direct[1];
+
+        if(map.get(x)[y] == 1){
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean checkMap(int[] location, int[] direct, int x, int y){
+        int[] tmp_location = {location[0],location[1],location[2]};
+        tmp_location[0] = tmp_location[0] + direct[0];
+        tmp_location[1] = tmp_location[1] + direct[1];
+
+        if( tmp_location[0] >= x && tmp_location[0] < 0){
+            return false;
+        }
+
+        if(tmp_location[1] >= y && tmp_location[1] < 0){
+            return false;
+        }
+
+        return true;
     }
 
     public static void turnLeft(int[] location){
