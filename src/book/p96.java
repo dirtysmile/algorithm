22e010be7fbd7372d47a10514300fe919c717fc9
dirtysmile <dirtysmile89@naver.com>
2021-05-10 08:05:00 +0900

    게임

--- src/book/p96.java
+++ src/book/p96.java
@@ -5,6 +5,17 @@ import java.util.Arrays;
 import java.util.List;
 
 public class p96 {
+    public static List<int[]> move = new ArrayList<>();
+
+    public static int[] dx = {-1,0,1,0};
+    public static int[] dy = {0,1,0,-1};
+
+    public static int[][] d = new int[50][50];
+    public static int[][] arr = new int[50][50];
+
+    public static int x,y,direction,n,m;
+
+
     public static void main(String[] args) {
         // location (x,y,캐릭터가 보고있는 방향)
         /*
@@ -14,106 +25,88 @@ public class p96 {
          * 2 : 남
          * 3 : 동
          */
-        int[] location = {1,1,0};
-        int x = 4;
-        int y = 4;
 
-        List<int[]> move = new ArrayList<>();
-        move.add(new int[]{0,1});
-        move.add(new int[]{-1,0});
-        move.add(new int[]{0,-1});
-        move.add(new int[]{1,0});
+        n = 4;
+        m = 4;
+        x = 1;
+        y = 1;
+        direction = 0;
 
-        List<int[]> map = new ArrayList();
-        map.add(new int[]{1,1,1,1});
-        map.add(new int[]{1,0,0,1});
-        map.add(new int[]{1,1,0,1});
-        map.add(new int[]{1,1,1,1});
+        d[x][y] = 1;
 
-        // 현재 위치 방문처리
-        visit(map,location);
+        arr[0][0] = 1;
+        arr[0][1] = 1;
+        arr[0][2] = 1;
+        arr[0][3] = 1;
 
-        boolean moveFlg = true;
-        while(moveFlg){
-            turnLeft(location);
-            switch (location[2]){
-                case 0 :
-                    boolean flg = moveMe(location, move.get(0), x, y, map);
-                    break;
-                case 1 :
-                    moveMe(location,move.get(1),x,y,map);
+        arr[1][0] = 1;
+        arr[1][1] = 0;
+        arr[1][2] = 0;
+        arr[1][3] = 1;
 
-                    break;
-                case 2 :
-                    moveMe(location,move.get(2),x,y,map);
-                    break;
-                case 3 :
-                    moveMe(location,move.get(3),x,y,map);
-                    break;
+        arr[2][0] = 1;
+        arr[2][1] = 1;
+        arr[2][2] = 0;
+        arr[2][3] = 1;
+
+        arr[3][0] = 1;
+        arr[3][1] = 1;
+        arr[3][2] = 1;
+        arr[3][3] = 1;
+
+        for(int i=0; i<4; i++){
+            for(int j=0; j<4; j++){
+                System.out.print(arr[i][j]+ " ");
+            }
+            System.out.println("");
+        }
+
+        int cnt = 1;
+        int turn_time = 0;
+
+        while (true){
+            turn_left();
+            int nx = x + dx[direction];
+            int ny = y + dy[direction];
+            // 회전한 이후 정면에 가보지 않은 칸이 존재하는 경우 이동
+            if (d[nx][ny] == 0 && arr[nx][ny] == 0) {
+                d[nx][ny] = 1;
+                x = nx;
+                y = ny;
+                cnt += 1;
+                turn_time = 0;
+                continue;
             }
 
-            moveFlg=false;
+
+            else turn_time += 1;
+            // 네 방향 모두 갈 수 없는 경우
+            if (turn_time == 4) {
+                nx = x - dx[direction];
+                ny = y - dy[direction];
+                // 뒤로 갈 수 있다면 이동하기
+                if (arr[nx][ny] == 0) {
+                    x = nx;
+                    y = ny;
+                }
+                // 뒤가 바다로 막혀있는 경우
+                else break;
+                turn_time = 0;
+            }
         }
 
-        for (Object o : map) {
-            System.out.println(o);
+        System.out.println("!!!!!!!!!");
 
-        }
-    }
+        System.out.println(cnt);
+        System.out.println("!!!!!!!!!");
 
-    public static boolean moveMe(int[] location, int[] direct, int x, int y, List map){
-        boolean check1 = checkMap(location,direct,x,y);
-
-        if(!check1){
-            return false;
-        }
-
-        boolean check2 = checkBottom(map,location,direct);
-
-        if(!check2){
-            return false;
-        }
-
-        return true;
-    }
-
-    public static boolean checkBottom(List<int []> map, int[] location, int[] direct){
-        int x = location[0] + direct[0];
-        int y = location[1] + direct[1];
-
-        if(map.get(x)[y] == 1){
-            return false;
-        }
-
-        return true;
-    }
-
-    public static boolean checkMap(int[] location, int[] direct, int x, int y){
-        int[] tmp_location = {location[0],location[1],location[2]};
-        tmp_location[0] = tmp_location[0] + direct[0];
-        tmp_location[1] = tmp_location[1] + direct[1];
-
-        if( tmp_location[0] >= x && tmp_location[0] < 0){
-            return false;
-        }
-
-        if(tmp_location[1] >= y && tmp_location[1] < 0){
-            return false;
-        }
-
-        return true;
-    }
-
-    public static void turnLeft(int[] location){
-        if(location[2]!=3){
-            location[2] = location[2] + 1;
-        }else{
-            location[2] = 0;
-        }
 
     }
 
-    public static void visit(List<int[]> map,int[] location){
-        map.get(location[0])[location[1]] = 1;
+    public static void turn_left() {
+        direction -= 1;
+        if (direction == -1) direction = 3;
     }
+
+
 }
