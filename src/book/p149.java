fdff2a1b5352f8d27e0b2d0dde55673b6e8c1b9e
thkim <dirtysmile89@naver.com>
2021-10-06 07:50:46 +0900

    p149 풀이

--- src/book/p149.java
+++ src/book/p149.java
@@ -1,7 +1,24 @@
 package book;
 
 public class p149 {
+
+    int[][] arr = {{1,1,0},{0,1,0},{0,1,1}};
     public static void main(String[] args) {
 
+
+        dfs(1,3);
+
+//        for(i)
+
+
+
+
+
+
+    }
+
+    public static boolean dfs(int x, int y){
+        System.out.println("dfs");
+        return false;
     }
 }
