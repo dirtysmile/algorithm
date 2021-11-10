0e10f886ad50502c0bf660ec513e6aba55a3bc69
thkim <dirtysmile89@naver.com>
2021-11-11 07:45:20 +0900

    p149 풀이

--- src/book/p149.java
+++ src/book/p149.java
@@ -1,8 +1,10 @@
 package book;
 
 public class p149 {
+    public static int n = 3;
+    public static int m = 3;
 
-    int[][] arr = {{1,1,0},{0,1,0},{0,1,1}};
+    public static int[][] graph = {{1,1,0},{0,1,0},{0,1,1}};
     public static void main(String[] args) {
 
 
@@ -18,6 +20,11 @@ public class p149 {
     }
 
     public static boolean dfs(int x, int y){
+
+        if(x<=-1 || x>=n || y <=-1 || y>=m){
+            return false;
+        }
+
         System.out.println("dfs");
         return false;
     }
